241a0fa1f65676a788eb15d941733b131ed665ff
giovannamandirola <69222964+giovannamandirola@users.noreply.github.com>
2020-10-01 19:04:13 -0300

    Enviar Mensajes por Twitter

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -18,6 +18,15 @@ namespace Program
             // Enviar un WhatsApp a algunos contactos
 
             // Enviar un SMS a algunos contactos
+
+            string consumerKey = "CONSUMERKEY";
+            string consumerKeySecret = "CONSUMERKEYSECRET";
+            string accessToken = "ACCESSTOKEN";
+            string accessTokenSecret = "ACCESSTOKENSECRET";
+            TwitterChannel twitter = new TwitterChannel(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
+            Message messageg = new Message("PooUcu", "809185528457338880");
+            messageg.Text = "Hola! Todo bien?";
+            twitter.Send(messageg);
         }
     }
 }
